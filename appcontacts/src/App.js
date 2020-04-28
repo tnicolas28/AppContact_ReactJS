7578c8d652cb3e27a89d2bb7bed117bbd97a9640
tnicolas28 <54882586+tnicolas28@users.noreply.github.com>
2020-04-29 01:31:58 +0200

    Update App.js

--- appcontacts/src/App.js
+++ appcontacts/src/App.js
@@ -4,7 +4,7 @@ import Liste from './composants/Contact/Liste'
 import AddContact from './composants/Contact/AddContact' 
 import {Provider} from './Context'
 import aPropos from './composants/Page/aPropos'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Erreur from './composants/Page/Erreur'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
